dde93f10566503f62f248d5de204571b897bbe96
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-06-04 21:04:23 -0500

    created image on screen from go

--- wasm/types.go
+++ wasm/types.go
@@ -11,6 +11,7 @@ type pixel struct {
 	red   uint8
 	green uint8
 	blue  uint8
+	alpha uint8
 }
 
 type canvas struct {
@@ -19,12 +20,22 @@ type canvas struct {
 	pixels    [][]pixel
 	imagedata js.Value
 	ctx       js.Value
+	element   js.Value
 }
 
 func (c *canvas) init() {
+	c.element = js.Global().Get("document").Call("getElementById", "canvas")
 	c.ctx = js.Global().Get("document").Call("getElementById", "canvas").Call("getContext", "2d")
 	c.width = c.ctx.Get("canvas").Get("width").Int()
 	c.height = c.ctx.Get("canvas").Get("height").Int()
+	c.setSizeFromDocument()
+	// c.setSize(700, 500)
+	// Setup resize listener
+	js.Global().Call("addEventListener", "resize", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		c.setSizeFromDocument()
+		c.render()
+		return nil
+	}))
 	c.imagedata = c.ctx.Call("createImageData", js.ValueOf(c.width), js.ValueOf(c.height))
 	c.pixels = make([][]pixel, c.height)
 	for i := range c.pixels {
@@ -33,17 +44,30 @@ func (c *canvas) init() {
 }
 
 func (c *canvas) setSize(w int, h int) {
-	c.ctx.Set("canvas", js.ValueOf(map[string]interface{}{
-		"width":  w,
-		"height": h,
-	}))
+	c.element.Set("width", w)
+	c.element.Set("height", h)
+	c.width = w
+	c.height = h
+}
+
+func (c *canvas) setSizeFromDocument() {
+	resolution := 4
+	c.width = js.Global().Get("window").Get("innerWidth").Int() / resolution
+	c.height = js.Global().Get("window").Get("innerHeight").Int() / resolution
+	c.setSize(c.width, c.height)
 }
 
 func (c *canvas) render() {
+	data := make([]uint8, c.width*c.height*4)
 	// loop over width and height pixels and add them to the imagedata array
-	for i := 0; i < c.height; i++ {
-		for j := 0; j < c.width; j++ {
-
-		}
+	totalPixels := c.width * c.height
+	for i := 0; i < totalPixels; i++ {
+		postition := i * 4
+		data[postition+0] = c.pixels[i/c.width][i%c.width].red
+		data[postition+1] = c.pixels[i/c.width][i%c.width].green
+		data[postition+2] = c.pixels[i/c.width][i%c.width].blue
+		data[postition+3] = c.pixels[i/c.width][i%c.width].alpha
 	}
+	js.CopyBytesToJS(c.imagedata.Get("data"), data)
+	c.ctx.Call("putImageData", c.imagedata, 0, 0)
 }
--- wasm/wasm.go
+++ wasm/wasm.go
@@ -3,7 +3,22 @@
 
 package main
 
+import (
+	"math/rand"
+)
+
 func main() {
-	InitBrowser()
+	canvasdata := canvas{}
+	canvasdata.init()
+	// set all pixels to red
+	for i := 0; i < canvasdata.height; i++ {
+		for j := 0; j < canvasdata.width; j++ {
+			randred := rand.Intn(256)
+			randgreen := rand.Intn(256)
+			randblue := rand.Intn(256)
+			canvasdata.pixels[i][j] = pixel{red: uint8(randred), green: uint8(randgreen), blue: uint8(randblue), alpha: 255}
+		}
+	}
+	canvasdata.render()
 	select {} // Keep the program running
 }
