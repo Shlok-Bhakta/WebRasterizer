fb64e2ea091c33279842e83c26f5b0971925b28a
Shlok Bhakta <shlokbhakta1@gmail.com>
2025-06-09 22:14:28 -0500

    animated triangle

--- wasm/wasm.go
+++ wasm/wasm.go
@@ -4,37 +4,45 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"time"
 )
 
 func main() {
 	canvasdata := canvas{}
 	canvasdata.init()
-	// set all pixels to red
-	for i := 0; i < canvasdata.height; i++ {
-		for j := 0; j < canvasdata.width; j++ {
-			canvasdata.pixels[i][j] = pixel{red: 255, green: 255, blue: 200}
-		}
-	}
-
-	// draw a triangle
-	triangle := triangle{
-		points: [3]point{
-			{x: 5, y: 10},
-			{x: 30, y: 40},
-			{x: 5, y: 80},
-		},
-		color: pixel{red: 255, green: 0, blue: 0},
-	}
-	// print the triangle points
-	fmt.Printf("Triangle points: %+v\n", triangle)
-	for i := 0; i < canvasdata.height; i++ {
-		for j := 0; j < canvasdata.width; j++ {
-			if triangle.is_inside(point{x: j, y: i}) {
-				canvasdata.pixels[i][j] = triangle.color
+	xval := 5
+	// render loop
+	for {
+		// set all pixels to red
+		for i := 0; i < canvasdata.height; i++ {
+			for j := 0; j < canvasdata.width; j++ {
+				canvasdata.pixels[i][j] = pixel{red: 255, green: 255, blue: 200}
 			}
 		}
+
+		// draw a triangle
+		triangle := triangle{
+			points: [3]point{
+				{x: 5, y: 10},
+				{x: 30, y: 40},
+				{x: xval, y: 80},
+			},
+			color: pixel{red: 255, green: 0, blue: 0},
+		}
+		// print the triangle points
+		// fmt.Printf("Triangle points: %+v\n", triangle)
+		for i := 0; i < canvasdata.height; i++ {
+			for j := 0; j < canvasdata.width; j++ {
+				if triangle.is_inside(point{x: j, y: i}) {
+					canvasdata.pixels[i][j] = triangle.color
+				}
+			}
+		}
+		canvasdata.render()
+		xval += 1
+		// just sleep for a bit
+		time.Sleep(time.Millisecond * 100)
 	}
-	canvasdata.render()
-	select {} // Keep the program running
+	// select {} // Keep the program running
 }
